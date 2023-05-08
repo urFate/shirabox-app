af619fed9de1421424d77dec85081352fe1c5d4a
urFate <georgiylakidon@gmail.com>
2023-05-08 13:56:47 +0300

    Use translation system & fix names

--- app/src/main/java/com/tomuki/tomuki/MainActivity.kt
+++ app/src/main/java/com/tomuki/tomuki/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-
-import androidx.compose.material.icons.outlined.Bookmark
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.BookmarkBorder
 import androidx.compose.material.icons.outlined.Explore
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,12 +19,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.tomuki.tomuki.ui.theme.TomukiTheme
-
 
 
 class MainActivity : ComponentActivity() {
@@ -51,19 +50,19 @@ class MainActivity : ComponentActivity() {
 fun Menu(){
     val bottomNavItems = listOf(
         BottomNavItem(
-            name = "Обзор",
+            name = stringResource(R.string.explore),
             route = "explore",
             icon = Icons.Outlined.Explore,
         ),
         BottomNavItem(
-            name = "Избранное",
-            route = "saved",
-            icon = Icons.Outlined.Bookmark,
+            name = stringResource(R.string.favourites),
+            route = "favourites",
+            icon = Icons.Outlined.BookmarkBorder,
         ),
         BottomNavItem(
-            name = "Настройки",
-            route = "settings",
-            icon = Icons.Outlined.Settings,
+            name = stringResource(R.string.profile),
+            route = "profile",
+            icon = Icons.Outlined.AccountCircle,
         ),
     )
 
