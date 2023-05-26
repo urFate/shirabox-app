258897ce70a87040ffd4a2e39261d708cf86b95f
urFate <georgiylakidon@gmail.com>
2023-05-26 22:35:38 +0300

    fix(PlayerLoadingStateListener): force to play video on media item transition

--- app/src/main/java/com/tomuki/tomuki/ui/activity/player/PlayerLoadingStateListener.kt
+++ app/src/main/java/com/tomuki/tomuki/ui/activity/player/PlayerLoadingStateListener.kt
@@ -2,25 +2,28 @@ package com.tomuki.tomuki.ui.activity.player
 
 import androidx.compose.runtime.MutableState
 import androidx.media3.common.Player
-import androidx.media3.exoplayer.ExoPlayer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class PlayerLoadingStateListener(
     private val coroutineScope: CoroutineScope,
-    private val player: ExoPlayer,
     private val controlsVisibilityState: MutableState<Boolean>
 ) : Player.Listener {
 
     override fun onIsLoadingChanged(isLoading: Boolean) {
         if(!isLoading){
             coroutineScope.launch {
-                delay(2000).let{
+                delay(1000).let{
                     controlsVisibilityState.value = false
                 }
             }
-            player.removeListener(this)
+        }
+    }
+
+    override fun onEvents(player: Player, events: Player.Events) {
+        if (events.contains(Player.EVENT_MEDIA_ITEM_TRANSITION)) {
+            player.playWhenReady = true
         }
     }
 }
