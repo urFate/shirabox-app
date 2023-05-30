1c598f9f27c016eb871eb216adada2c4214ccd18
urFate <georgiylakidon@gmail.com>
2023-05-30 17:25:47 +0300

    fixup! refactor(Player): move constants to Values.kt

--- app/src/main/java/com/tomuki/tomuki/util/Values.kt
+++ app/src/main/java/com/tomuki/tomuki/util/Values.kt
@@ -2,7 +2,7 @@ package com.tomuki.tomuki.util
 
 class Values {
     companion object {
-        val INSTANT_SEEK_TIME = 10000
-        val CONTROLS_HIDE_DELAY = 3000L
+        const val INSTANT_SEEK_TIME = 10000
+        const val CONTROLS_HIDE_DELAY = 3000L
     }
 }
