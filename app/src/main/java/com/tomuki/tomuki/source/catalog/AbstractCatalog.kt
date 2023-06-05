0460e282e06e5aa834427ea76e900e19005bedef
urFate <georgiylakidon@gmail.com>
2023-06-05 12:42:29 +0300

    refactor(AbstractCatalog): move network job to JSoup

--- app/src/main/java/com/tomuki/tomuki/source/catalog/AbstractCatalog.kt
+++ app/src/main/java/com/tomuki/tomuki/source/catalog/AbstractCatalog.kt
@@ -1,10 +1,13 @@
 package com.tomuki.tomuki.source.catalog
 
+import android.util.Log
 import com.tomuki.tomuki.model.Content
 import com.tomuki.tomuki.model.ContentType
 import com.tomuki.tomuki.util.Values
 import okhttp3.OkHttpClient
-import okhttp3.Request
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
 
 abstract class AbstractCatalog internal constructor(
     val name: String, val url: String, private val client: OkHttpClient = OkHttpClient()
@@ -16,15 +19,25 @@ abstract class AbstractCatalog internal constructor(
     abstract suspend fun search(query: String, type: ContentType): List<Content>
 
     fun httpGET(url: String): String? {
-        val request = Request.Builder().apply {
-            this.url(url)
-            this.header("User-Agent", Values.USER_AGENT)
-        }.build()
-
-        client.newCall(request).execute().use {
-            if (!it.isSuccessful) return null
-
-            return it.body?.string()
+        val document: Document = try {
+            Jsoup.connect(url)
+                .ignoreHttpErrors(true)
+                .followRedirects(true)
+                .ignoreContentType(true)
+                .userAgent(Values.USER_AGENT)
+                .timeout(15000)
+                .get()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
         }
+
+        val response = document.connection().response()
+        if(response.statusCode() != 200) {
+            Log.e("HttpGET", "Failed to GET $url: ${response.statusCode()} - ${response.statusMessage()}")
+            return null
+        }
+
+        return document.wholeText()
     }
 }
