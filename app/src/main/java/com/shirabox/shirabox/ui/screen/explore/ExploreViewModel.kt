879773a14c60d1e6989d2924e087aa383c4d0df2
urFate <georgiylakidon@gmail.com>
2023-07-29 13:58:27 +0300

    refactor(ExploreViewModel): reimagine business logic

--- app/src/main/java/com/shirabox/shirabox/ui/screen/explore/ExploreViewModel.kt
+++ app/src/main/java/com/shirabox/shirabox/ui/screen/explore/ExploreViewModel.kt
@@ -1,7 +1,8 @@
 package com.shirabox.shirabox.ui.screen.explore
-
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.shirabox.shirabox.model.Content
@@ -11,51 +12,38 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ExploreViewModel : ViewModel() {
-    val animeOngoings = mutableStateOf<List<Content>>(emptyList())
-    val animePopulars = mutableStateOf<List<Content>>(emptyList())
+    private val animeOngoings = mutableStateListOf<Content>()
+    private val animePopulars = mutableStateListOf<Content>()
 
-    val mangaOngoings = mutableStateOf<List<Content>>(emptyList())
-    val mangaPopulars = mutableStateOf<List<Content>>(emptyList())
+    private val mangaOngoings = mutableStateListOf<Content>()
+    private val mangaPopulars = mutableStateListOf<Content>()
 
-    val ranobeOngoings = mutableStateOf<List<Content>>(emptyList())
-    val ranobePopulars = mutableStateOf<List<Content>>(emptyList())
+    private val ranobeOngoings = mutableStateListOf<Content>()
+    private val ranobePopulars = mutableStateListOf<Content>()
 
-    fun fetchOngoings(page: Int, contentType: ContentType){
+    var currentContentType by mutableStateOf(ContentType.ANIME)
+
+    internal fun currentOngoings() = when(currentContentType) {
+        ContentType.ANIME -> animeOngoings
+        ContentType.MANGA -> mangaOngoings
+        ContentType.RANOBE -> ranobeOngoings
+    }
+
+    internal fun currentPopulars() = when(currentContentType) {
+        ContentType.ANIME -> animePopulars
+        ContentType.MANGA -> mangaPopulars
+        ContentType.RANOBE -> ranobePopulars
+    }
+
+    fun fetchOngoings(page: Int) {
         viewModelScope.launch(Dispatchers.IO) {
-            when(contentType){
-                ContentType.ANIME -> animeOngoings.value = Shikimori.fetchOngoings(page, contentType)
-                ContentType.MANGA -> mangaOngoings.value = Shikimori.fetchOngoings(page, contentType)
-                ContentType.RANOBE -> ranobeOngoings.value = Shikimori.fetchOngoings(page, contentType)
-            }
+            currentOngoings().addAll(Shikimori.fetchOngoings(page, currentContentType))
         }
     }
 
-    fun fetchPopulars(page: Int, contentType: ContentType){
+    fun fetchPopulars(page: Int) {
         viewModelScope.launch(Dispatchers.IO) {
-            val contents = Shikimori.fetchPopulars(page, contentType)
-
-            when(contentType){
-                ContentType.ANIME -> compareAndCollect(contents, animePopulars)
-                ContentType.MANGA -> compareAndCollect(contents, mangaPopulars)
-                ContentType.RANOBE -> compareAndCollect(contents, ranobePopulars)
-            }
-
-        }
-    }
-
-    private fun compareAndCollect(contents: List<Content>, state: MutableState<List<Content>>){
-        state.value =
-            when {
-                contents != state.value -> state.value.plus(contents)
-                else -> state.value
-            }
-    }
-
-    fun isReady(contentType: ContentType) : Boolean {
-        return when(contentType){
-            ContentType.ANIME -> animeOngoings.value.isNotEmpty() && animePopulars.value.isNotEmpty()
-            ContentType.MANGA -> mangaOngoings.value.isNotEmpty() && mangaPopulars.value.isNotEmpty()
-            ContentType.RANOBE -> ranobeOngoings.value.isNotEmpty() && ranobePopulars.value.isNotEmpty()
+            currentPopulars().addAll(Shikimori.fetchPopulars(page, currentContentType))
         }
     }
 }
