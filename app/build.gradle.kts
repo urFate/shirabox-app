9ab397ed3f12b65eb052d17246a4fb04e82375c5
urFate <georgiylakidon@gmail.com>
2024-04-07 12:13:35 +0300

    chore(build): bump compose to 1.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,33 +59,33 @@ android {
 }
 
 dependencies {
-
     // Core dependencies
+    val composeVersion = "1.6.5"
+
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation ("androidx.activity:activity-compose:1.8.2")
-    implementation ("androidx.compose.ui:ui:1.6.4")
-    implementation ("androidx.compose.ui:ui-graphics:1.6.4")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.6.4")
+    implementation ("androidx.compose.ui:ui:$composeVersion")
+    implementation ("androidx.compose.ui:ui-graphics:$composeVersion")
+    implementation ("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation (platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation (platform("androidx.compose:compose-bom:2024.04.00"))
     implementation (platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.6.4")
-    androidTestImplementation (platform("androidx.compose:compose-bom:2024.03.00"))
-    debugImplementation ("androidx.compose.ui:ui-tooling:1.6.4")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.6.4")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    androidTestImplementation (platform("androidx.compose:compose-bom:2024.04.00"))
+    debugImplementation ("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:$composeVersion")
 
     // Compose UI
     implementation ("de.mr-pine.utils:zoomables:1.4.0")
     implementation ("androidx.compose.material3:material3:1.2.1")
-    implementation ("androidx.compose.material:material-icons-extended:1.6.4")
+    implementation ("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
     implementation ("com.google.accompanist:accompanist-placeholder-material3:0.30.1")
