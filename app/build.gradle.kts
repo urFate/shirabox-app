41dc636ac46737e177ae20f3d3208a309f7241f4
urFate <georgiylakidon@gmail.com>
2024-04-22 22:16:03 +0300

    chore: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,25 +60,24 @@ android {
 
 dependencies {
     // Core dependencies
-    val composeVersion = "1.6.5"
+    val composeVersion = "1.6.6"
 
-    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.core:core-ktx:1.13.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation ("androidx.activity:activity-compose:1.8.2")
+    implementation ("androidx.activity:activity-compose:1.9.0")
     implementation ("androidx.compose.ui:ui:$composeVersion")
     implementation ("androidx.compose.ui:ui-graphics:$composeVersion")
     implementation ("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation (platform("androidx.compose:compose-bom:2024.04.00"))
-    implementation (platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
+    implementation (platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation (platform("org.jetbrains.kotlin:kotlin-bom:1.9.23"))
     implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    androidTestImplementation (platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation (platform("androidx.compose:compose-bom:2024.04.01"))
     debugImplementation ("androidx.compose.ui:ui-tooling:$composeVersion")
     debugImplementation ("androidx.compose.ui:ui-test-manifest:$composeVersion")
 
@@ -87,7 +86,7 @@ dependencies {
 
     implementation ("de.mr-pine.utils:zoomables:1.4.0")
     implementation ("androidx.compose.material3:material3:1.2.1")
-    implementation ("androidx.compose.material:material:1.6.5")
+    implementation ("androidx.compose.material:material:1.6.6")
     implementation ("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
@@ -101,7 +100,7 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     // Exo Player
-    val media3Version = "1.3.0"
+    val media3Version = "1.3.1"
 
     implementation ("androidx.media3:media3-exoplayer:$media3Version")
     implementation ("androidx.media3:media3-exoplayer-dash:$media3Version")
@@ -117,18 +116,18 @@ dependencies {
     implementation ("androidx.room:room-ktx:$roomVersion")
 
     // Firebase
-    implementation (platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation (platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation ("com.google.firebase:firebase-crashlytics-ktx")
     implementation ("com.google.firebase:firebase-analytics-ktx")
     implementation ("com.google.firebase:firebase-messaging-ktx")
 
     // Network
-    implementation ("io.coil-kt:coil-compose:2.4.0")
+    implementation ("io.coil-kt:coil-compose:2.5.0")
     implementation("com.github.kittinunf.fuel:fuel:3.0.0-alpha1")
     implementation("io.coil-kt:coil-svg:2.6.0")
 
     // Datastore
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation ("androidx.datastore:datastore-preferences:1.1.0")
 
     // Project modules
     implementation (project(":core"))
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -43,6 +43,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("androidx.room:room-common:2.6.1")
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
-    implementation ("androidx.media3:media3-common:1.3.0")
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation ("androidx.media3:media3-common:1.3.1")
+    implementation ("androidx.datastore:datastore-preferences:1.1.0")
 }
