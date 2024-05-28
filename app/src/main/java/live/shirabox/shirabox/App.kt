b0b937d30815822c31a9dfd2666a7c22afd60572
urFate <georgiylakidon@gmail.com>
2024-05-28 21:26:23 +0300

    refactor: remove memory cache policy

--- app/src/main/java/live/shirabox/shirabox/App.kt
+++ app/src/main/java/live/shirabox/shirabox/App.kt
@@ -4,21 +4,15 @@ import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.disk.DiskCache
-import coil.memory.MemoryCache
 
 class App : Application(), ImageLoaderFactory {
 
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
-            .memoryCache {
-                MemoryCache.Builder(this)
-                    .maxSizePercent(0.25)
-                    .build()
-            }
             .diskCache {
                 DiskCache.Builder()
                     .directory(cacheDir.resolve("image_cache"))
-                    .maxSizeBytes(5 * 1024 * 1024)
+                    .maxSizePercent(0.02)
                     .build()
             }
             .build()
