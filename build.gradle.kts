946bc292943277c09eaa87ba7e3f613b94b26875
urFate <georgiylakidon@gmail.com>
2024-08-22 18:31:57 +0300

    chore: 1.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     kotlin("android") version "1.9.20" apply false
-    id ("com.android.application") version "8.5.1" apply false
-    id ("com.android.library") version "8.5.1" apply false
+    id ("com.android.application") version "8.5.2" apply false
+    id ("com.android.library") version "8.5.2" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("androidx.room:room-common:2.6.1")
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
     implementation ("androidx.media3:media3-common:1.4.0")
