f4f38357a4ea66e6561c7d93e87b4cdb643c496d
urFate <georgiylakidon@gmail.com>
2024-08-26 12:56:13 +0300

    chore: 1.2.31 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.2.3"
+        versionName = "1.2.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
