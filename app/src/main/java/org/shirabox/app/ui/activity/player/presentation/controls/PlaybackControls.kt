110908f747944cd6ea06b7fe5f856490f3ba6408
george <georgiylakidon@gmail.com>
2024-09-15 12:14:18 +0300

    refactor(Player): make playback button bigger

--- app/src/main/java/org/shirabox/app/ui/activity/player/presentation/controls/PlaybackControls.kt
+++ app/src/main/java/org/shirabox/app/ui/activity/player/presentation/controls/PlaybackControls.kt
@@ -3,23 +3,29 @@ package org.shirabox.app.ui.activity.player.presentation.controls
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Pause
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.SkipNext
 import androidx.compose.material.icons.rounded.SkipPrevious
+import androidx.compose.material.ripple
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -81,10 +87,24 @@ internal fun PlaybackControls(
 }
 
 @Composable
-private fun PlaybackIconButton(isActive: Boolean = true, imageVector: ImageVector, onClick: () -> Unit) {
-    IconButton(
-        onClick = onClick,
-        enabled = isActive
+private fun PlaybackIconButton(
+    isActive: Boolean = true,
+    imageVector: ImageVector,
+    onClick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        modifier = Modifier
+            .size(64.dp)
+            .clip(RoundedCornerShape(100))
+            .clickable(
+                interactionSource = interactionSource,
+                enabled = isActive,
+                indication = ripple(bounded = true, radius = 100.dp, color = Color.White),
+                onClick = onClick
+            ),
+        contentAlignment = Alignment.Center
     ) {
         Icon(
             modifier = Modifier.size(42.dp),
