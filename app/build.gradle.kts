f591f792407a691f99976fa46480e0e9c12a6a13
george <georgiylakidon@gmail.com>
2024-09-18 17:40:44 +0300

    chore: 1.2.41 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.2.4"
+        versionName = "1.2.41"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
