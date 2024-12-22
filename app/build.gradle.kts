9988671406470b7b3554134af41c3c6c9b9b93d0
urFate <georgiylakidon@gmail.com>
2024-12-23 00:13:30 +0300

    chore: 1.3.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.2.42"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
