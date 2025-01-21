b998177e7ccbc595b5c1cc0a5643bd84af09f732
urFate <georgiylakidon@gmail.com>
2025-01-21 15:39:38 +0300

    chore: update version name to 1.3.21
    Updated the app version name from 1.3.20 to 1.3.21.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.3.20"
+        versionName = "1.3.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
